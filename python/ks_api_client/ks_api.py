b84bd42af5fa32265dcf036bea6b14338b04a597
Nishanth D Aluhonnu <nishanthdilip@yahoo.co.in>
2022-05-11 15:36:08 +0530

    Update ks_api.py

--- python/ks_api_client/ks_api.py
+++ python/ks_api_client/ks_api.py
@@ -391,10 +391,10 @@ class KSTradeApi():
                     reconnection=True, request_timeout=20, reconnection_attempts=5, engineio_logger=False,
                             logger=False,http_session=session, ssl_verify=session.verify)
 
-                #def setInterval(func,time):
-                #    e = threading.Event()
-                #    while not e.wait(time):
-                #        func()   
+                def setInterval(func,time):
+                    e = threading.Event()
+                    while not e.wait(time):
+                        func()   
 
                 def foo():
                     self.sio.emit('handshake', {'inputtoken': 'Hello World!'})                 
@@ -403,7 +403,7 @@ class KSTradeApi():
                 def connect():
                     # print("Connection success")
                     self.sio.emit('pageload', {'inputtoken': input_tokens})
-                    # setInterval(foo,5)
+                    setInterval(foo,5)
 
                 @self.sio.event
                 def connect_error(data):
@@ -414,6 +414,16 @@ class KSTradeApi():
                 def disconnect():
                     #print('Connection closed')
                     pass
+
+                @self.sio.on('broadcast')
+                def on_broadcast(msg):
+                    # print('broadcast: ', msg)
+                    pass
+
+                @self.sio.on('message')
+                def on_message(msg):
+                    # print('message: ', msg)
+                    pass   
 
                 @self.sio.on('getdata')
                 def on_getdata(data, callback=callback):
